dbf927b75198a8391d0a40b7a9a734c403d6a5c7
BhalchandraDesai <Bhalchandra.Desai@assetsoft.biz>
2024-06-19 17:30:01 +0530

    Added Created Date

--- DevOpsTest/dbo/Tables/employee_Mst.sql
+++ DevOpsTest/dbo/Tables/employee_Mst.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[employee_Mst] (
     [ID]       INT           IDENTITY (1, 1) NOT NULL,
     [Emp_Name] VARCHAR (100) NULL,
-    [Emp_Add]  VARCHAR (500) NULL
+    [Emp_Add]  VARCHAR (500) NULL, 
+    [CreatedDate] DATETIME NULL
 );
 
